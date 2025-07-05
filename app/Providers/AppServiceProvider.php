c5f9ac9e1e5b99c88a60b3e28fe8afcbb887d491
Adrian Glazer <adrianbadjideh@student.telkomuniversity.ac.id>
2025-07-05 10:48:32 +0700

    add update procfile

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,7 +19,7 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void
+    public function boot(UrlGenerator $url): void
     {
         //
         Config::set('fortify.home', '/dashboard');

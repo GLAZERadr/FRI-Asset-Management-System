4f8bf1b2bde570c13074fe394a44ba60ecabf07b
Adrian Glazer <adrianbadjideh@student.telkomuniversity.ac.id>
2025-07-06 14:03:04 +0700

    push with cloudinary storage

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use App\Http\Middleware\ForceHttps; 
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -13,8 +12,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         //
-        $middleware->append(ForceHttps::class);
-        
         $middleware->alias([
             'role' => CheckRole::class,
         ]);
